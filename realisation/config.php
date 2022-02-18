ac34820d217f9886ccc5a692f1e2881e9fa19c65
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-02-18 16:59:29 +0000

    Add config

---
+++ realisation/config.php
@@ -0,0 +1,9 @@
+<?php
+    $connect = mysqli_connect("localhost", "maskoul", "test123", "employee_db");
+
+    if(!$connect){
+        echo "Connect error: " . mysqli_connect_error();
+    }
+
+    //column names id 	employee_id 	first_name 	last_name 	birth_date 	salary 	fonction 	photo
+?>
