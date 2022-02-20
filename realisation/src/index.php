0280ae0ba167ae95687de582f0d4375e8846d9f7
maskoul-mohamed <moha1058@gmail.com>
2022-02-20 14:58:46 +0100

    Add home page

---
+++ realisation/src/index.php
@@ -0,0 +1,54 @@
+<?php
+    include 'config.php';
+    include 'employeeManager.php';
+
+    $employeeManager = new EmployeeManager();
+    $employees = $employeeManager->getAllEmployees($conn);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <a href="insert.php">Insert Data</a>
+        <table>
+            <tr>
+                <th>Employee Id</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Birth Date</th>
+                <th>Salary</th>
+                <th>Fonction</th>
+                <th>Photo</th>
+            </tr>
+
+            <?php
+                    foreach($employees as $employee){
+            ?>
+
+            <tr>
+                <td><?= $employee['employee_id']?></td>
+                <td><?= $employee['first_name']?></td>
+                <td><?= $employee['last_name']?></td>
+                <td><?= $employee['birth_date']?></td>
+                <td><?= $employee['salary']?></td>
+                <td><?= $employee['fonction']?></td>
+                <td><img src="<?php echo $employee['photo']; ?>"></td>
+                <td>
+                    <a href="edit.php?id=<?php echo $employee['id'] ?>">Edit</a>
+                    <a href="delete.php?id=<?php echo $employee['id'] ?>">delete</a>
+                </td>
+            </tr>
+            <?php }?>
+        </table>
+    </div>
+</body>
+</html>
