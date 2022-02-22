2c5635a65251efbba33c0174a86aeaaa25f1ff47
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-02-22 15:58:48 +0000

    Delete conncet to data base function

--- prototypes/prototype 4/employeeManager.php
+++ prototypes/prototype 4/employeeManager.php
@@ -3,19 +3,7 @@
 
     class EmployeeManager {
 
-        private $Connection = null;
 
-        private function getConnection(){
-            if(is_null($this->Connection)){
-                $this->Connection = mysqli_connect('localhost', 'maskoul', 'test123', 'employees_db');
-
-                if(!$this->Connection){
-                    $message = 'Connection Error: ' .mysqli_connect_error();
-                    throw new Exception($message);
-                }
-            }
-            return $this->Connection;
-        }
 
         public function getAllEmployees(){
        
