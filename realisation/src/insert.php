fb9333db82a1130d671cc1bc725825da12e5a05a
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-02-25 12:29:54 +0000

    Edit nav brand

--- realisation/src/insert.php
+++ realisation/src/insert.php
@@ -42,7 +42,7 @@
 <body>
 <nav class="navbar navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand">Employees Manager</a>
+            <a href="index.php" class="navbar-brand btn">Employees Manager</a>
         </div>
     </nav>
 
@@ -50,7 +50,7 @@
         <div class="row flex-nowrap">
             <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
                 <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                    <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <a href="index.php" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                         <span class="fs-5 d-none d-sm-inline">Menu</span>
                     </a>
                     <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
