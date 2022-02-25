4770d34b26d20692664eb5762a4594628651b726
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-02-25 12:22:53 +0000

    Delete search field in add & edit page

--- realisation/src/edit.php
+++ realisation/src/edit.php
@@ -60,10 +60,6 @@
 <nav class="navbar navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand">Employees Manager</a>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success text-white" type="submit">Search</button>
-            </form>
         </div>
     </nav>
 
