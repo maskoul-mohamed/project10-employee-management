c6ddfff44f7bf8b64a9c67ea3a76b827f4af8781
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-03-08 12:36:41 +0000

    test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    include 'employeeManager.php';
+    include 'src/employeeManager.php';
     session_start();
     $employeeManager= new EmployeeManager();
 
@@ -11,7 +11,7 @@
         }
 
     } else {
-        header("Location: signin.php");
+        header("Location: src/signin.php");
     }
 
 
@@ -33,7 +33,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="src/styles/styles.css">
 </head>
 
 <body>
